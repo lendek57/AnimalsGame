97eaf4bb25df3e71f657a0c39389c0ce712caec0
Lena Dekster <lenadekster@gmail.com>
2025-11-16 08:33:47 +0100

    test

--- src/main/java/com/ggit/Plant.java
+++ src/main/java/com/ggit/Plant.java
@@ -1,7 +1,10 @@
 package com.ggit;
 
+import java.util.List;
+
 public class Plant {
     private Vector2D position;
+    private List test;
 
     public Plant(Vector2D position) {
         this.position = position;
